c06526580f939222613ec5f0e99788038bdaabc8
jmkaz16 <jmkaz16@gmail.com>
2025-03-30 18:42:32 +0200

    Añadir proyecto general

---
+++ main.c
@@ -0,0 +1,18 @@
+/*
+ * AtraccionDeFeria.c
+ *
+ * Created: 30/03/2025 18:34:49
+ * Author : juanm
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
