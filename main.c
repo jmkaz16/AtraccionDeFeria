56e38781911eb25bb2ab129eab6729bd85389640
claudiaadsuara <cadsuara@outlook.es>
2025-04-04 17:38:10 +0200

    Añadir configuración puertos en main.c

--- main.c
+++ main.c
@@ -2,11 +2,31 @@
  * AtraccionDeFeria.c
  *
  * Created: 30/03/2025 18:34:49
- * Author : juanm
+ * Author : juanm & cloti
  */ 
 
 #include <avr/io.h>
-// Holap, me permite subir cosas?
+#include <avr/interrupt.h>
+#include "pinout.h"
+
+void setup(){
+	
+	// Deshabilitar interrupciones
+	cli();
+	
+	// Configurar LEDs como salida
+	DDRL |= (1 << B_L3)	
+	DDRL |= (1 << B_L4)	
+	
+	// Configurar motores como salida
+	DDRL |= (1 << B_EN2)	
+	DDRL |= (1 << B_DI2)
+	
+	// Configurar 	
+	
+	// Habilitar interrupciones
+	sei();
+}
 
 
 int main(void)
--- pinout.h
+++ pinout.h
@@ -1,24 +1,26 @@
 #ifndef PINOUT_H
 #define PINOUT_H
 
-// Puertos y bits de los motores
-#define P_M2
-#define B_M2
+// Puertos y bits de las señales motores
+#define P_EN2 PORTL
+#define B_EN2 2
+#define P_DI2 PORTL
+#define B_DI2 3
 
 // Puertos y bits de los sensores opticos
-#define P_SO3
-#define B_SO3
-#define P_SO4
-#define B_SO4
+#define P_SO3 PORTB
+#define B_SO3 0
+#define P_SO4 PORTB
+#define B_SO4 1
 
 // Puertos y bits de los sensores mecanicos
-#define P_SW1
-#define B_SW1
+#define P_SW1 PORTD
+#define B_SW1 0
 
 // Puertos y bits de los LEDs
-#define P_L3
-#define B_L3
-#define P_L4
-#define B_L4
+#define P_L3 PORTL
+#define B_L3 0
+#define P_L4 PORTL
+#define B_L4 1
 
 #endif  // PINOUT_H
