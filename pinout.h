695e3977870146b61c727b42cf2ffc8917abc8c9
jmkaz16 <jmkaz16@gmail.com>
2025-04-04 13:02:50 +0200

    Añadir pinout.h

---
+++ pinout.h
@@ -0,0 +1,24 @@
+#ifndef PINOUT_H
+#define PINOUT_H
+
+// Puertos y bits de los motores
+#define P_M2
+#define B_M2
+
+// Puertos y bits de los sensores opticos
+#define P_SO3
+#define B_SO3
+#define P_SO4
+#define B_SO4
+
+// Puertos y bits de los sensores mecanicos
+#define P_SW1
+#define B_SW1
+
+// Puertos y bits de los LEDs
+#define P_L3
+#define B_L3
+#define P_L4
+#define B_L4
+
+#endif  // PINOUT_H
