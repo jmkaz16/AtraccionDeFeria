ec68817fd8b1800521611c9d3366414358ee045b
rafa-robotics <r.bonmatidela@alumnos.upm.es>
2025-04-13 17:10:50 +0200

    Primera definicion pins y entradas salidas
    Usando la base de juanm Y cloti hemos definido nuestro pinout propio atendiendo a la tabla de Word de One Drive y configurado en el main las entradas y las salidas

--- main.c
+++ main.c
@@ -1,12 +1,42 @@
 /*
  * AtraccionDeFeria.c
  *
- * Created: 30/03/2025 18:34:49
- * Author : juanm
+ * Created: 13/04/2025 17:10:27
+ * Author : rodri & rafa
  */ 
 
 #include <avr/io.h>
-// Holap, me permite subir cosas?
+#include <avr/interrupt.h>
+#include "pinout.h"
+
+void setup(){
+	
+	// Deshabilitar interrupciones
+	cli();
+	
+	// Configurar LEDs como salida
+	DDRL |= (1 << B_L2)	
+	
+	// Configurar motores como salida
+	DDRL |= (1 << B_EN1)	
+	DDRL |= (1 << B_BK1)
+	
+	// Configurar interrupciones PCINT
+	PCICR |= (1 << PCIE0)
+	
+	// Habilitar mascaras sensores opticos
+	PCMSK0 |= (1 << B_SO2) // PCINT2?
+	PCMSK0 |= (1 << B_SO3) // PCINT3?
+	
+	// Configurar interrupcion INT0 (flanco de subida)
+	EICRA |= (1 << 2*B_SW2) | (1 << 2*B_SW2+1)				// CONFIRMAR PRIMERO!!!
+	
+	// Habilitar mascara sensor mecanico
+	EIMSK |= (1 << B_SW2)
+	
+	// Habilitar interrupciones
+	sei();
+}
 
 
 int main(void)
---
+++ pinout.h
@@ -0,0 +1,26 @@
+// SO2, SO3, SW2, L2, M1
+
+#ifndef PINOUT_H_MONEDERO
+#define PINOUT_H_MONEDERO
+
+// Puertos y bits de las señales motores
+#define P_EN1 PORTL
+#define B_EN1 5
+#define P_BK1 PORTL
+#define B_BK1 6
+
+// Puertos y bits de los sensores opticos
+#define P_SO2 PORTB
+#define B_SO2 2
+#define P_SO3 PORTB
+#define B_SO3 3
+
+// Puertos y bits de los sensores mecanicos
+#define P_SW2 PORTD
+#define B_SW2 1
+
+// Puertos y bits de los LEDs
+#define P_L2 PORTL
+#define B_L2 4
+
+#endif  // PINOUT_H_MONEDERO
