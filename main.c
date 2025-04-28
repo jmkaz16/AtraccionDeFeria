74606de74f6b88bc8f8dc30392aedb9cc6b4b3a5
jmkaz16 <jmkaz16@gmail.com>
2025-04-28 11:41:52 +0200

    Función parpadeo() v2

--- main.c
+++ main.c
@@ -16,54 +16,29 @@
 
 uint16_t t_total = 0;      // Tiempo total de parpadeo en decimas de segundo
 uint16_t t_encendido = 0;  // Tiempo de parpadeo en decimas de segundo
-uint16_t last_encendido = 0;
-uint16_t last_total = 0;
+volatile uint16_t parpadeo_cnt = 0;
 
-volatile uint64_t millis100_cnt = 0;  // Contador de 100ms
-
-// ISR del timer 4 (millis100)
+// ISR del timer 4 (cadan 100ms)
 ISR(TIMER4_COMPA_vect) {
-    millis100_cnt++;
-}
-
-uint64_t millis100() {
-    return millis100_cnt;
+    parpadeo_cnt++;
 }
 
 void parpadeo() {
-    //if (millis100() - last_total >= t_total) {
-        //last_total = millis100();
-    //} else {
-        //if (millis100() - last_encendido >= t_encendido) {
-            //last_encendido = millis100();
-            //clrBit(P_L4, B_L4);  // Apagar LED 4
-        //} else {
-			//setBit(P_L4, B_L4);  // Encender LED 4
-        //}
-    //}
-	// Si t_encendido == t_total, mantener encendido siempre
-	if (t_encendido >= t_total) {
-		setBit(P_L4, B_L4);
-		return;
-	}
-
-	// Si se completó un ciclo, reiniciamos los tiempos
-	if (millis100() - last_total >= t_total) {
-		last_total = millis100();
-		last_encendido = millis100();
-	}
-
-	// Determinar si estamos dentro del tiempo de encendido o apagado
-	if (millis100() - last_encendido < t_encendido) {
-		setBit(P_L4, B_L4);  // Encender LED
-		} else {
-		clrBit(P_L4, B_L4);  // Apagar LED
-	}
+    if (parpadeo_cnt < t_total) {
+        if (parpadeo_cnt < t_encendido) {
+            setBit(P_L4, B_L4);  // Encender LED 4
+        } else {
+            clrBit(P_L4, B_L4);  // Apagar LED 4
+        }
+    } else {
+        parpadeo_cnt = 0;
+    }
 }
 
+// Configurar tiempos de parpadeo en ms
 void setParpadeo(uint16_t t_encendido_ms, uint16_t t_total_ms) {
-    t_encendido = t_encendido_ms/100;  // Convertir a decimas de segundo
-    t_total = t_total_ms/100;          // Convertir a decimas de segundo
+    t_encendido = t_encendido_ms / 100;  // Convertir a decimas de segundo
+    t_total = t_total_ms / 100;          // Convertir a decimas de segundo
 }
 
 void setup() {
@@ -113,11 +88,9 @@ void setup() {
 }
 
 int main(void) {
-    setup();                  // Configuracion inicial
-    setParpadeo(10, 100);  // Encender 500ms y apagar 10000ms
+    setup();                 // Configuracion inicial
+    setParpadeo(500, 1000);  // Encender 500ms y apagar 10000ms
     while (1) {
         parpadeo();  // Llamar a la funcion de parpadeo
-		
     }
 }
-
