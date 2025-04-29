ddb91933eaf6ada2a5712ed753e73da12b389d62
CeliaHdez <celia.hdevergas@alumnos.upm.es>
2025-04-29 20:36:58 +0200

    Lectura_bits.c con millis y distingue nueva, guardada o no válida
    He incluido una función en la distingue entre tarjeta válida o no y si es válida entre guardada o nueva. SI es nueva la guarda. Además enciende el led dependiendo de cada caso de una forma distinta. Lo hace con la función millis (que ya la he puesto con su configuración del timer3).

--- Lectura_bits.c
+++ Lectura_bits.c
@@ -10,12 +10,15 @@ volatile uint8_t flanco_index = 0;
 volatile uint8_t captura_completa = 0;
 volatile uint32_t desbordamiento=0;
 volatile uint32_t tiempo=0;
+long int tiempo_total=0;
+
 
 volatile uint8_t valores[24] = {0};
 
 char tarjeta[6];
 
-char usuarios[6]=":+097-"; //Cadena resultante de la tarjeta
+char usuarios[10][6]={":+097-"}; //Tarjetas guardadas
+volatile uint8_t num_usarios=1;    //Numero de tarjetas guardadas inicialmente
 
 
 char tabla_codigos[16]={ //Tabla para la decodificacion de bits a caracteres
@@ -37,6 +40,21 @@ void setup() {
 	TCCR1B = (1 << ICES1) | (1 << CS11); // Flanco de subida inicialmente, prescaler 8
 	TIMSK1 |= (1 << ICIE1); // Habilitar interrupción input capture
 	TIMSK1 |= (1<<TOIE1); //Habilitar interrupcion por desbordamiento
+	
+	//Configuración TIMER 3 para millis
+	// Timer 3 en modo CTC (Top OCRnA)
+	TCCR3A &= ~(1 << WGM30);
+	TCCR3A &= ~(1 << WGM31);
+	TCCR3B |= (1 << WGM32);
+	TCCR3B &= ~(1 << WGM33);
+
+	// Mascara para no tener preescalado
+	TCCR3B = (TCCR3B | (1 << CS30));
+	TCCR3B = (TCCR3B & (~(1 << CS31)));
+	TCCR3B = (TCCR3B & (~(1 << CS32)));
+	
+	OCR3A = 8000;
+	
 	sei();
 }
 
@@ -57,6 +75,16 @@ ISR(TIMER1_CAPT_vect) {
 ISR(TIMER1_OVF_vect){
 	desbordamiento++;
 }
+
+
+long int milis(){
+	return tiempo_total;
+}
+
+ISR(TIMER3_COMPA_vect){
+	tiempo_total++;
+}
+
 
 void procesar_tarjeta() {
 	if (!captura_completa) return;
@@ -80,7 +108,8 @@ void procesar_tarjeta() {
 	// 	}
 	
 	convertidor_bits_a_numero(valores, tarjeta);
-	comparar_tarjeta(usuarios, tarjeta);
+	//comparar_tarjeta(usuarios, tarjeta);
+	gestionar_tarjeta();
 
 	// Reset para próxima tarjeta
 	flanco_index = 0;
@@ -114,6 +143,60 @@ void comparar_tarjeta(const char* usuario, const char* leida) {
 	PORTL |= (1 << PL7);   // Encender LED si coinciden
 	else
 	PORTL &= ~(1 << PL7);  // Apagar LED si no coinciden
+}
+
+void gestionar_tarjeta(){
+	 uint32_t tiempo_inicial;
+	 uint32_t tiempo_actual;
+	 uint32_t duracion_espera;
+	 uint8_t encontrada = 0;
+	 
+	  if (!tarjeta_valida(tarjeta)) {
+		  tiempo_inicial = millis();
+		  do {
+			  tiempo_actual = millis();
+			  if ((tiempo_actual - tiempo_inicial) % 200 < 100) {  //Entre 200 porque es el periodo: 100 encendido y 100 apagado
+				    PORTL |= (1 << PL7);  // LED ON
+				    } else {
+				    PORTL &= ~(1 << PL7); // LED OFF
+			    }
+			 } while (tiempo_actual - tiempo_inicial < 1000);
+			  PORTL &= ~(1 << PL7);    //Apagamos LED definitivamente
+			  
+		 } else {
+			  // Comprobamos si la tarjeta está en la lista de usuarios
+			  for (uint8_t i = 0; i < num_usuarios && !encontrada; i++) {
+				  if (strcmp(tarjeta, usuarios[i]) == 0) {
+					  encontrada = 1;
+				  }
+			  }
+			   if (encontrada) {
+				   duracion_espera = 1000;
+				   } else {
+				   duracion_espera = 3000;
+				   //Añadimos la tarjeta puesto que no estaba en la lista
+				    if (num_usuarios < 10) {
+					    strcpy(usuarios[num_usuarios], tarjeta);
+					    num_usuarios++;
+				    }
+			   }
+			   
+			   tiempo_inicial = millis();
+			   PORTL |= (1 << PL7);		//Encendemos led
+			   do {
+				   tiempo_actual = millis();
+			   } while (tiempo_actual - tiempo_inicial < duracion_espera);	//Apaga tanto si encontrada como no, varian solo los segundos
+			   PORTL &= ~(1 << PL7);
+		}
+}
+
+
+uint8_t tarjeta_valida(const char* t) {
+	for (uint8_t i = 0; i < 6; i++) {
+		if (t[i] == '?' || t[i] == '\0') return 0;		//Menos de 6 caracteres o caracter '?'
+	}
+	if (t[6] != '\0') return 0; // Más de 6 caracteres
+	return 1;
 }
 
 int main(void) {
