8faff7a6b549159f66f1220300330b42542b1e39
jmkaz16 <jmkaz16@gmail.com>
2025-05-08 18:05:52 +0200

    Implementar tarjetero

--- main.c
+++ main.c
@@ -6,19 +6,21 @@
  */
 
 #include "atraccion.h"
+#include "tarjetero.h"
 
 volatile bool emergencia_flag = false;  // Bandera de emergencia
 
-
-void setup(){
-	atraccionSetup(); // Configuracion inicial de la atraccion
+void setup() {
+    atraccionSetup();  // Configuracion inicial de la atraccion
+    tarjeteroSetup();  // Configuracion inicial del tarjetero
 }
 
 int main(void) {
     setup();
     while (1) {
         if (!emergencia_flag) {
-            atraccion();  // Llamar a la funcion de la atraccion
+            atraccion();        // Llamar a la funcion de la atraccion
+            procesarTarjeta();  // Llamar a la funcion de procesar tarjeta
         }
         parpadeo();  // Llamar a la funcion de parpadeo
     }
