505300c60a7f04b279d1bcdce1e3bb928fd04d4c
jmkaz16 <jmkaz16@gmail.com>
2025-05-08 20:09:58 +0200

    Modificar comentarios

--- atraccion.h
+++ atraccion.h
@@ -10,22 +10,22 @@
 
 #include "pinout.h"
 
-extern volatile bool emergencia_flag;  // Bandera de emergencia
+extern volatile bool emergencia_flag;  // bandera de emergencia
 
-void atraccionSetup();  // Configuracion de entradas, salidas, interrupciones y temporizadores de la atraccion
-void atraccion();       // Funcion principal de la atraccion (consulta periodica en el main)
+void atraccionSetup();  // configuracion de entradas, salidas, interrupciones y temporizadores de la atraccion
+void atraccion();       // funcion principal de la atraccion (consulta periodica en el main)
 
-void setAtraccion();    // Activar la atraccion
-void clrAtraccion();    // Desactivar la atraccion
-void moverAtraccion();  // Mover la atraccion
+void setAtraccion();    // activar la atraccion
+void clrAtraccion();    // desactivar la atraccion
+void moverAtraccion();  // mover la atraccion
 
-void setParpadeo(uint16_t t_encendido_ms, uint16_t t_total_ms);  // Configurar tiempos de parpadeo en ms
-void parpadeo();                                                 // Funcion para activar parpadeo del LED 4
+void setParpadeo(uint16_t t_encendido_ms, uint16_t t_total_ms);  // configurar tiempos de parpadeo en ms
+void parpadeo();                                                 // funcion para activar parpadeo del LED 4
 
 ISR(INT0_vect);    // ISR del pulsador de emergencia SW1 (INT0)
 ISR(INT2_vect);    // ISR del sensor optico 4 (INT2)
 ISR(INT3_vect);    // ISR del sensor optico 5 (INT3)
-ISR(PCINT2_vect);  // // ISR del sensor mecánico SW3 (PCINT20)
+ISR(PCINT2_vect);  // ISR del sensor mecánico SW3 (PCINT20)
 
 ISR(TIMER0_COMPA_vect);  // ISR del temporizador 0 (Timer 0) (cada 1ms)
 ISR(TIMER4_COMPA_vect);  // ISR del temporizador 4 (Timer 4) (cada 100ms)
