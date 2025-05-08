d5b18c7bc5a3ab4b3f360c7e136e1504ab12a3db
jmkaz16 <jmkaz16@gmail.com>
2025-05-08 19:22:24 +0200

    Arreglar millis()

--- tarjetero.h
+++ tarjetero.h
@@ -12,12 +12,15 @@
 
 extern volatile uint8_t personas_cnt;
 
+uint32_t millis();
+
 void tarjeteroSetup();
 uint8_t tarjetaValida(const char* t);
 void gestionarTarjeta();
 void compararTarjeta(const char* usuario, const char* leida);
 void conversorBits2Numero(volatile uint8_t* vector, char* tarjeta);
 void procesarTarjeta();
+
 ISR(TIMER1_CAPT_vect);
 ISR(TIMER1_OVF_vect);
 ISR(TIMER3_COMPA_vect);
