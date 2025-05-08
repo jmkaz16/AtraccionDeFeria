eb6ddb69800b368e1a47fa21292f9c933d270c32
jmkaz16 <jmkaz16@gmail.com>
2025-05-08 18:06:17 +0200

    Renombrar funcion

--- atraccion.h
+++ atraccion.h
@@ -3,7 +3,6 @@
 
 #include <avr/interrupt.h>
 #include <avr/io.h>
-
 #include <stdbool.h>
 #include <stdint.h>
 #include <stdlib.h>
@@ -13,8 +12,8 @@
 
 extern volatile bool emergencia_flag;  // Bandera de emergencia
 
-void atraccion_setup();  // Configuracion de entradas, salidas, interrupciones y temporizadores de la atraccion
-void atraccion();        // Funcion principal de la atraccion (consulta periodica en el main)
+void atraccionSetup();  // Configuracion de entradas, salidas, interrupciones y temporizadores de la atraccion
+void atraccion();       // Funcion principal de la atraccion (consulta periodica en el main)
 
 void setAtraccion();    // Activar la atraccion
 void clrAtraccion();    // Desactivar la atraccion
@@ -22,5 +21,13 @@ void moverAtraccion();  // Mover la atraccion
 
 void setParpadeo(uint16_t t_encendido_ms, uint16_t t_total_ms);  // Configurar tiempos de parpadeo en ms
 void parpadeo();                                                 // Funcion para activar parpadeo del LED 4
+
+ISR(INT0_vect);    // ISR del pulsador de emergencia SW1 (INT0)
+ISR(INT2_vect);    // ISR del sensor optico 4 (INT2)
+ISR(INT3_vect);    // ISR del sensor optico 5 (INT3)
+ISR(PCINT2_vect);  // // ISR del sensor mecánico SW3 (PCINT20)
+
+ISR(TIMER0_COMPA_vect);  // ISR del temporizador 0 (Timer 0) (cada 1ms)
+ISR(TIMER4_COMPA_vect);  // ISR del temporizador 4 (Timer 4) (cada 100ms)
 
 #endif  // ATRACCION_H
